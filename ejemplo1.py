85ea5c93e435caa3ddd99ede87ee061daf7bf261
A01745913 <kpaori@ASUS-KP.localdomain>
2020-10-29 13:05:33 -0600

    Venom chikito

--- ejemplo1.py
+++ ejemplo1.py
@@ -4,10 +4,20 @@ imagen=cv2.imread("imagen.jpg")
 imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2RGB)
 
 print(imagen.shape)
-print(imagen[0][0][0])
+print(imagen[0][0])
 imagen = cv2.resize(imagen,(256,256))
 
 cv2.imwrite("resizeimagen.jpg",imagen)
-cv2.imshow("image",imagen)
-cv2.waitKey(0)
+
+
+imagen = cv2.imread("imagen.jpg")
+imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+print(image.shape)
+print(imagen)
+cv2.imwrite("grayimagen.jpg",imagen)
+
+#cv2.imshow("image",imagen)
+#cv2.waitKey(0)
+
+
 
